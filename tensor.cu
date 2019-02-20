491c0adaa24d6d502d41ec8364808497128a2b9f
maomran <mohamed.omran4192@gmail.com>
2019-02-19 16:39:14 -0800

    update

--- tensor.cu
+++ tensor.cu
@@ -1,7 +1,51 @@
-class tensor
+#include "tensor.h"
+
+__global__ void tensorAdd(const float *A, const float *B, float *C, int m, int n)
 {
-public:
-	tensor();
-	~tensor();
-	
-};
+    int row = blockDim.x * blockIdx.x + threadIdx.x;
+    int col = blockIdx.y * blockDim.y + threadIdx.y;
+    if (row < m && col < n)
+    {
+        C[row*m + col] = A[row*m + col] + B[row*m + col];
+    }
+}
+
+__global__ void tensorSub(const float *A, const float *B, float *C, int m, int n)
+{
+    int row = blockDim.x * blockIdx.x + threadIdx.x;
+    int col = blockIdx.y * blockDim.y + threadIdx.y;
+    if (row < m && col < n)
+    {
+        C[row*m + col] = A[row*m + col] - B[row*m + col];
+    }
+}
+
+
+__global__
+void kScale(float *a, float factor, int sizeX, int sizeY) {
+    int x = blockIdx.x * blockDim.x + threadIdx.x;
+    int y = blockIdx.y * blockDim.y + threadIdx.y;
+    if (x < sizeX && y < sizeY) {
+        a[y*sizeX + x] *= factor;
+    }
+}
+
+__global__
+void kMultiply(int fieldsPerBlockX, int fieldsPerBlockY, int fieldsPerThreadX, int fieldsPerThreadY,
+               float* A, int aX, int aY,
+               float* B, int bX, int bY,
+               float* C){
+    int row = blockIdx.y * blockDim.y + threadIdx.y; 
+    int col = blockIdx.x * blockDim.x + threadIdx.x;
+    int i;
+    float sum = 0;
+    if( col < k && row < m) 
+    {
+        for( i = 0; i < n; i++) 
+        {
+            sum += A[row * n + i] * B[i * k + col];
+        }
+        C[row * k + col] = sum;
+    }
+
+}
--- tensor.h
+++ tensor.h
@@ -3,6 +3,10 @@
 
 #include <cstdio>
 
+struct tensor
+{
+	
+};
 class tensor
 {
 public:
@@ -12,8 +16,20 @@ public:
 
 	tensor(int idx, int idy);
 	tensor(int idx, int idy, float *d_data);
-	tensor(int idx, int idym float **h_data);
+	tensor(int idx, int idy, float **h_data);
 	~tensor();
+
+    float* Host2Dev();
+    float** Dev2Host();
+    
+    void add(Tensor2D* tensor);
+    void subtract(Tensor2D* tensor);
+    void scale(float factor);
+    Tensor2D* multiply(Tensor2D* tensor, Tensor2D* output);
+    Tensor2D* multiplyByTransposition(Tensor2D* tensor, Tensor2D* output);
+    Tensor2D* transposeAndMultiply(Tensor2D* tensor, Tensor2D* output);
+    Tensor1D* avg(Tensor1D* output);
+
 	
 };
 #endif
